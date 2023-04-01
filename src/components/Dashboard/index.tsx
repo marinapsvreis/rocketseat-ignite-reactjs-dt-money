2583eee73940ed281d22ef1343da1fc66e04c218
Marina Portugal <marinapsvreis@gmail.com>
2023-04-01 20:49:59 -0300

    :lipstick: Tabela de transactions (entradas e saidas)

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from '../Summary'
 import { Container } from "../Dashboard/styles";
+import { TransactionsTable } from '../TransactionsTable';
 
 export function Dashboard(){
     return(
         <Container>
             <Summary />
+            <TransactionsTable/>
         </Container>
     );
 }
