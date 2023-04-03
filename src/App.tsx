95971005f643ce56b0d205d812ecd4750e7cca1b
Marina Portugal <marinapsvreis@gmail.com>
2023-04-03 19:29:56 -0300

    :sparkles: Aprendendo a usar contexto para corregar a lista de transações

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { GlobalStyle } from './styles/global';
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 export function App() {
@@ -18,7 +19,7 @@ export function App() {
   }
   
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
       <NewTransactionModal
@@ -26,6 +27,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
